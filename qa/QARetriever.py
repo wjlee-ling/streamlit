9e67282d362255fa79e8729bec542348e3c63a6d
wjlee <nlpotato31@gmail.com>
2023-10-04 10:34:23 +0900

    [Refactor] using class

--- qa/QARetriever.py
+++ qa/QARetriever.py
@@ -1,4 +1,5 @@
 import argparse
+from typing import Optional
 from langchain.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
@@ -8,51 +9,53 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 
 
-loader = WebBaseLoader("https://textnet.kr/about")
-data = loader.load()
-# loaders = [....]
-# data = []
-# for loader in loaders:
-#     data.extend(loader.load())
+class QARetriever:
+    # prompt engineering
+    template = """Observe the following rules to answer the question at the end.\
+    1. Answer the question in a complete sentence.\
+    2. Answer in Korean.\
+    3. Answer in a polite manner with honorifics. \
+    4. If you don't know the answer, just type "잘 모르겠습니다".\
+    5. DO NOT swear or use offensive language.\
+    {context}\
+    question: {question}\
+    answer:"""
+    prompt = PromptTemplate.from_template(template)
 
-text_spliter = RecursiveCharacterTextSplitter(chunk_size=150, chunk_overlap=0)
-splits = text_spliter.split_documents(data)
-vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+    # extract (distill) the retrieved documents into an answer using LLM/Chat model
+    llm = ChatOpenAI(
+        model_name="gpt-4",
+        temperature=0,
+    )
 
-# extract (distill) the retrieved documents into an answer using LLM/Chat model
-llm = ChatOpenAI(
-    model_name="gpt-4",
-    temperature=0,
-)
+    def __init__(self, url:str):
+        loader = WebBaseLoader(url)
+        data = loader.load()
+        text_spliter = RecursiveCharacterTextSplitter(chunk_size=150, chunk_overlap=0)
+        splits = text_spliter.split_documents(data)
+        vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
 
-# prompt engineering
-template = """Observe the following rules to answer the question at the end.\
-1. Answer the question in a complete sentence.\
-2. Answer in Korean.\
-3. Answer in a polite manner with honorifics. \
-4. If you don't know the answer, just type "잘 모르겠습니다".\
-5. DO NOT swear or use offensive language.\
-{context}\
-question: {question}\
-answer:"""
-prompt = PromptTemplate.from_template(template)
+        self.qa_chain = RetrievalQA.from_chain_type(
+            # chain_type: 
+            # "stuff": default; to use all of the text from the documents in the prompt
+            # "map_reduce": to batchify docs and feeds each batch with the question to LLM, and come up with the final answer based on the answers
+            # "refine": to batchify docs and feeds the first batch to LLM, and then feeds the second batch with the answer from the first one, and so on
+            # "map-rerank": to batchify docs and feeds each batch, return a score and come up with the final answer based on the scores
+            self.llm,
+            retriever=vectorstore.as_retriever(), 
+            chain_type_kwargs={"prompt": self.prompt}, 
+        )
 
-qa_chain = RetrievalQA.from_chain_type(
-    # chain_type: 
-    # "stuff": default; to use all of the text from the documents in the prompt
-    # "map_reduce": to batchify docs and feeds each batch with the question to LLM, and come up with the final answer based on the answers
-    # "refine": to batchify docs and feeds the first batch to LLM, and then feeds the second batch with the answer from the first one, and so on
-    # "map-rerank": to batchify docs and feeds each batch, return a score and come up with the final answer based on the scores
-    llm,
-    retriever=vectorstore.as_retriever(), 
-    chain_type_kwargs={"prompt": prompt}, 
-)
+    def __call__(self, query:str):
+        return self.qa_chain({"query": query})
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run a basic QA Retriever powered by ChatGPT-4')
     parser.add_argument('-q', '--question', type=str, required=True)
+    parser.add_argument('-u', '--url', type=str, default="https://textnet.kr/about")
 
     args = parser.parse_args()
-    question = args.question
-    res = qa_chain({"query": question})
+    bot = QARetriever(url=args.url)
+    
+    res = bot(query=args.question) # {"query": args.question}
     print(res)
