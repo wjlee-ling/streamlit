06e9554620c7ed856b3d150923df9266c992d8fb
wjlee <nlpotato31@gmail.com>
2023-10-04 14:50:44 +0900

    [Update] introduce ConversationalRetrievalChain and Memory for multi-turn

--- qa/retriever.py
+++ qa/retriever.py
@@ -5,9 +5,10 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
+from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
-
+from langchain.memory import ConversationBufferMemory
 
 class QARetriever:
     # prompt engineering
@@ -28,14 +29,21 @@ class QARetriever:
         temperature=0,
     )
 
+    # memory for chat history
+    memory = ConversationBufferMemory(
+        memory_key="chat_history",
+        return_messages=True,
+    )
+
     def __init__(self, url:str):
         loader = WebBaseLoader(url)
         data = loader.load()
-        text_spliter = RecursiveCharacterTextSplitter(chunk_size=150, chunk_overlap=0)
+        text_spliter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=10)
         splits = text_spliter.split_documents(data)
         vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
 
-        self.qa_chain = RetrievalQA.from_chain_type(
+
+        self.qa_chain = ConversationalRetrievalChain.from_llm(
             # chain_type: 
             # "stuff": default; to use all of the text from the documents in the prompt
             # "map_reduce": to batchify docs and feeds each batch with the question to LLM, and come up with the final answer based on the answers
@@ -43,11 +51,12 @@ class QARetriever:
             # "map-rerank": to batchify docs and feeds each batch, return a score and come up with the final answer based on the scores
             self.llm,
             retriever=vectorstore.as_retriever(), 
-            chain_type_kwargs={"prompt": self.prompt}, 
+            # chain_type_kwargs={"prompt": self.prompt}, 
+            memory=self.memory,
         )
 
     def __call__(self, query:str):
-        return self.qa_chain({"query": query})
+        return self.qa_chain({"question": query})
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run a basic QA Retriever powered by ChatGPT-4')
