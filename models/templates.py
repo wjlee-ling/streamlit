18828a8af438fbf318e1afaa32625f8ccebe2752
wjlee <nlpotato31@gmail.com>
2023-10-12 08:59:18 +0900

    new: template

---
+++ models/templates.py
@@ -0,0 +1,24 @@
+rules = """[규칙] \
+1. [답변]은 온전한 문장으로 작성하라. \
+2. [답변]은 한국어로 작성하라. \
+3. [답변]은 반말은 사용하지 말고 존댓말로 격식있게 하라. \
+4. [답변]을 모르겠다면 "잘 모르겠습니다."라고 작성하라. \
+5. 욕, 비속어, 인종차별, 성차별 기타 소수자 혐오 발언은 하지 말라.\
+"""
+
+template_en = """Observe the following rules to answer the question at the end.\
+    1. Answer the question in a complete sentence.\
+    2. Answer in Korean.\
+    3. Answer in a polite manner with honorifics. \
+    4. If you don't know the answer, just type "잘 모르겠습니다".\
+    5. DO NOT swear or use offensive language.\
+    Given the rules, the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
+    chat history: {chat_history}\
+    question: {question}\
+    answer:"""
+
+template_kor = """{rules} \
+[대화내역] {chat_history} \
+[질문] {question} \
+위 [규칙]과 [대화내역]을 참고하여 [질문]에 대한 [답변]을 작성하라.\
+[답변] """
