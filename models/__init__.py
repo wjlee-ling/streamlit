b7277809d2416f42f415b26eece2c3404a4bf5ae
wjlee <nlpotato31@gmail.com>
2023-10-12 11:04:01 +0900

    update: misc.

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,2 @@
+from .templates import rules, template_en, template_kor
 from .qaretriever import QARetriever
--- models/templates.py
+++ models/templates.py
@@ -3,7 +3,7 @@ rules = """[규칙] \
 2. [답변]은 한국어로 작성하라. \
 3. [답변]은 반말은 사용하지 말고 존댓말로 격식있게 하라. \
 4. [답변]을 모르겠다면 "잘 모르겠습니다."라고 작성하라. \
-5. 욕, 비속어, 인종차별, 성차별 기타 소수자 혐오 발언은 하지 말라.\
+5. [답변]에 욕, 비속어, 인종차별, 성차별 기타 소수자 혐오 발언은 하지 말라.\
 """
 
 template_en = """Observe the following rules to answer the question at the end.\
@@ -20,5 +20,5 @@ template_en = """Observe the following rules to answer the question at the end.\
 template_kor = """{rules} \
 [대화내역] {chat_history} \
 [질문] {question} \
-위 [규칙]과 [대화내역]을 참고하여 [질문]에 대한 [답변]을 작성하라.\
+위 [규칙]과 [대화내역]을 참고하여 [질문]에 대한 [답변]을 작성하라. \
 [답변] """
