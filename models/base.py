70a779f98ee78b0de33f9c64238ecd27ef6eba2d
wjlee <nlpotato31@gmail.com>
2023-10-13 15:36:28 +0900

    refactor: enable custom-prompt for each combine-doc-chain-type

--- models/base.py
+++ models/base.py
@@ -1,8 +1,8 @@
-from .templates import rules, template_en, template_kor
+from .templates import CustomPrompts, rules, template_en, template_kor
 from utils import create_collection
 
 import langchain
-from typing import Optional, Any, Dict
+from typing import Optional, Any, Dict, Union
 from langchain.schema import BaseDocumentTransformer
 from langchain.schema.prompt_template import BasePromptTemplate
 from langchain.schema.language_model import BaseLanguageModel
@@ -15,25 +15,43 @@ from langchain.cache import InMemoryCache
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
+from pydantic import BaseModel
 
+
+class CustomPrompts(BaseModel):
+    """
+    Prompts for each chain type: 'stuff', 'map_reduce', 'refine', 'map-rerank'
+    Refer to [langchain.chains.question_answering](https://github.com/langchain-ai/langchain/tree/c2d1d903fa35b91018b4d777db2b008fcbaa9fbc/langchain/chains/question_answering) for default prompts.
+    """
+    
+    condense_question_prompt: BasePromptTemplate  # for first question condesing w/ context
+    qa_prompt: BasePromptTemplate  # for final answer generation
+    combine_prompt: Optional[BasePromptTemplate] = None  # for "map_reduce"
+    collapse_prompt: Optional[BasePromptTemplate] = None # for "map_reduce"
+    refine_prompt: Optional[BasePromptTemplate] = None  # for "refine"
 
 class BaseBot:
     langchain.llm_cache = InMemoryCache()
 
     def __init__(
         self,
-        prompt: Optional[BasePromptTemplate] = None,
+        prompts: Optional[CustomPrompts] = None,
         llm: Optional[BaseLanguageModel] = None,
-        vectorstore: Optional[VectorStore] = None,
         condense_question_llm: Optional[BaseLanguageModel] = None,
+        vectorstore: Optional[VectorStore] = None,
+        docs_chain_type: str = "stuff",
+        docs_chain_kwargs: Optional[Any] = None,
         configs: Optional[Dict[str, Any]] = None,
     ) -> None:
-        self.prompt = (
-            prompt
-            if prompt
-            else PromptTemplate.from_template(template_kor.replace("{rules}", rules))
-        )
-
+        """
+        Args:
+            - prompts: dict of prompts to use for each chain type. If not given, default prompts will be used. Different sets of prompts are required for different chain types.
+            For example, `stuff` chain_type requires `qa_prompt` and `condense_question_prompt` prompts, while `map_reduce` chain_type requires `condense_question_prompt`, `question_prompt` and `combine_prompt` prompts.
+        """
+        #prompts
+        _, self.docs_chain_kwargs = self._validate_docs_chain_and_prompts(prompts, docs_chain_type, docs_chain_kwargs)
+        
+        # llm for doc-chain
         self.llm = (
             ChatOpenAI(
                 model_name="gpt-4",
@@ -73,14 +91,41 @@ class BaseBot:
             # "map-rerank": to batchify docs and feeds each batch, return a score and come up with the final answer based on the scores
             self.llm,
             retriever=self.retriever,
-            # chain_type_kwargs={"prompt": self.prompt},
             memory=self.memory,
+            chain_type=docs_chain_type,
             condense_question_llm=self.condense_question_llm,
-            condense_question_prompt=self.prompt,
+            condense_question_prompt=self.condense_question_prompt,
+            combine_docs_chain_kwargs=self.docs_chain_kwargs,
+            rephrase_question=True, # default: True; Will pass the new generated question for retrieval
+            return_generated_questions=False
+            return_source_documents=True,
+            get_chat_history=None, # default: None -> will use default;
         )
 
     def __call__(self, question: str):
         return self.chain(question)
+
+    def _validate_docs_chain_and_prompts(self, prompts, docs_chain_type:str, docs_chain_kwargs:Dict):
+        assert docs_chain_type in ["stuff", "map_reduce", "refine", "map-rerank"], \
+        f"docs_chain_type must be one of ['stuff', 'map_reduce', 'refine', 'map-rerank'], but got {docs_chain_type}"
+        
+        if docs_chain_type == "stuff":
+            assert prompts.combine_prompt is None and prompts.collapse_prompt is None and prompts.refine_prompt is None
+            prompts["prompt"] = prompts.pop("qa_prompt")
+        elif docs_chain_type == "map-rerank":
+            assert prompts.combine_prompt is None and prompts.collapse_prompt is None and prompts.refine_prompt is None
+            prompts["prompt"] = prompts.pop("qa_prompt")
+        elif docs_chain_type == "refine":
+            assert prompts.refine_prompt and prompts.collapse_prompt is None and prompts.combine_prompt is None
+            prompts["question_prompt"] = prompts.pop("qa_prompt")
+        else:
+            assert prompts.refine_prompt is None and prompts.collapse_prompt and prompts.combine_prompt
+            prompts["question_prompt"] = prompts.pop("qa_prompt")
+
+        self.condense_question_prompt = prompts.pop("condense_question_prompt")
+        docs_chain_kwargs.update(prompts)
+
+        return prompts, docs_chain_kwargs
 
     @staticmethod
     def __configure__(configs: Dict[str, Any]):
@@ -108,9 +153,11 @@ class BaseBot:
         loader: BaseLoader,
         splitter: Optional[BaseDocumentTransformer] = None,
         collection_name: str = "default",
-        prompt: Optional[BasePromptTemplate] = None,
         llm: Optional[BaseLanguageModel] = None,
         condense_question_llm: Optional[BaseLanguageModel] = None,
+        prompts: Optional[CustomPrompts] = None,
+        docs_chain_type: str = "stuff",
+        docs_chain_kwargs: Optional[Any] = None,
         configs: Optional[Dict[str, Dict[str, str]]] = None,
     ):
         """Build new collection AND chain based on it"""
@@ -126,9 +173,11 @@ class BaseBot:
             docs=docs,
         )
         return cls(
-            prompt=prompt,
+            prompts=prompts,
             llm=llm,
             vectorstore=vectorstore,
             condense_question_llm=condense_question_llm,
+            docs_chain_type=docs_chain_type,
+            docs_chain_kwargs=docs_chain_kwargs,
             configs=configs,
         )
--- models/templates.py
+++ models/templates.py
@@ -1,16 +1,18 @@
 """
 chain 타입에 따라 template의 포맷과 variable이 달라짐.
 `ConversationalRetrievalChain`의 경우, quenstion-condensing을 위한 `CONDENSE_QUESTION_PROMPT` template과 answer-generation을 위한 `QA_PROMPT` template이 필요함. (https://github.com/langchain-ai/langchain/blob/master/libs/langchain/langchain/chains/conversational_retrieval/prompts.py#L10)
-`CONDENSE_QUESTION_PROMPT` template의 경우, `chat_history`와 `question`을 variable로, `QA_PROMPT`의 경우 `question`과 `context`를 variable로 사용함.
-
+`ConversationalRetrievalChain.from_llm()`의 경우 
+1. `CONDENSE_QUESTION_PROMPT` template은 인자 `condense_question_prompt`로 커스텀할 수 있으며, default template은 `langchain.chains.conversational_retrieval.prompts.py`에 지정되어 있음.
+2. `QA_PROMPT`는 `from_llm()` 내부 `load_qa_chain()`에서 `chain_type`("stuff" by default)을 받고 그 종류에 따라 `langchain.chains.question_answering`에 지정된 default prompt를 사용함.
+    커스텀하려면 `combine_docs_chain_kwargs`에 해당 prompt_type 명시해야함 (참고: https://github.com/langchain-ai/langchain/blob/c2d1d903fa35b91018b4d777db2b008fcbaa9fbc/langchain/chains/question_answering/__init__.py#L134)
 """
 
 rules = """[규칙] \
-1. [답변]은 온전한 문장으로 작성하라. \
-2. [답변]은 한국어로 작성하라. \
-3. [답변]은 반말은 사용하지 말고 존댓말로 격식있게 하라. \
-4. [답변]을 모르겠다면 "잘 모르겠습니다."라고 작성하라. \
-5. [답변]에 욕, 비속어, 인종차별, 성차별 기타 소수자 혐오 발언은 하지 말라.\
+1. [최종 답변]은 온전한 문장으로 작성하라. \
+2. [최종 답변]은 한국어로 작성하라. \
+3. [최종 답변]은 반말은 사용하지 말고 존댓말로 격식있게 하라. \
+4. [최종 답변]을 모르겠다면 "잘 모르겠습니다."라고 작성하라. \
+5. [최종 답변]에 욕, 비속어, 인종차별, 성차별 기타 소수자 혐오 발언은 하지 말라.\
 """
 
 template_en = """Observe the following rules to answer the question at the end.\
@@ -29,3 +31,16 @@ template_kor = """{rules} \
 [질문] {question} \
 위 [규칙]과 [대화내역]을 참고하여 [질문]에 대한 [답변]을 작성하라. \
 [답변] """
+
+condense_question_template = (
+    "[대화 내역]을 기반으로 [사용자 질문]을 고치거나 보강하여 한국어로 [수정된 사용자 질문]을 작성하라",
+    "[대화 내역] {chat_history}",
+    "[사용자 질문] {question}",
+    "[수정된 사용자 질문] ",
+)
+
+stuff_qa_template = (
+    "[질문]과 [context]를 바탕으로 [최종 답변]을 작성하라. [최종 답변] 작성시 아래 [규칙]을 참고하라.",
+    rules,
+    "[최종 답변] ",
+)
