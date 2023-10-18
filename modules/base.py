ec6f6ca47ae72278d43574165457ff90b7e0f078
wjlee <nlpotato31@gmail.com>
2023-10-19 00:57:23 +0900

    running w/ new API Key checked

--- modules/base.py
+++ modules/base.py
@@ -65,7 +65,7 @@ class BaseBot:
         # llm for doc-chain
         self.llm = (
             ChatOpenAI(
-                model_name="gpt-4",
+                model_name="gpt-3.5-turbo-0613",  # "gpt-4"
                 temperature=0,
                 verbose=True,
             )
