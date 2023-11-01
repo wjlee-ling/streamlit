c7b2711fc3943e862bd31fe0cda9213335ef6efa
wjlee <nlpotato31@gmail.com>
2023-11-01 18:26:09 +0900

    add: new Notion DataLoader and Preprocessor to folder init

--- modules/preprocessors/__init__.py
+++ modules/preprocessors/__init__.py
@@ -1,3 +1,3 @@
 from modules.preprocessors.base import BasePreprocessor
 from modules.preprocessors.pdf import PDFPreprocessor
-from modules.preprocessors.notion import NotionPreprocessor
+from modules.preprocessors.notion import NotionPreprocessor, NotionDataLoader
